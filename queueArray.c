77b8554b144e7f1438a1c4c0c55093687e2ab39d
Salo Soja Edwin <salosoja@gmail.com>
2022-12-11 00:48:44 +0530

    Added Queue with Array

---
+++ queueArray.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#define size 20
+int queue[size];
+int front = -1;
+int rear = -1;
+
+void enqueue(int x){
+    if(rear==size-1){
+        printf("Queue Overflow\n");
+    }else if(front==-1 && rear==-1){
+        front=rear=0;
+        queue[rear]=x;
+    }else{
+        rear++;
+        queue[rear]=x;
+    }
+}
+
+void dequeue(){
+    if(front == -1 && rear == -1){
+        printf("Queue Underflow\n");
+    }else if(front == rear){
+        front = rear = -1;
+    }else{
+        printf("Deleted %d from front\n",queue[front]);
+        front++;
+    }
+}
+
+void display(){
+    if(front ==-1 && rear == -1){
+        printf("Queue Empty\n");
+    }else{
+        printf("Queue\n");
+        for(int i=front; i<rear+1;i++){
+            printf("%d ",queue[i]);
+        }
+    }
+}
+
+void main(){
+    int loop=1;
+    while(loop){
+        int choice;
+        printf("\nWhich operation would you like to do in queue?\n");
+        printf("1 : Inserte data in the rear\n");
+        printf("2 : Delete data in the front\n");
+        printf("3 : Display the queue\n");
+        printf("4 : Exit\n");
+        printf("Your choice: ");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+            printf("How many elements would you like to insert: ");
+            int num,i=0,data; 
+            scanf("%d",&num);
+            while(i<num){
+                printf("Enter term %d: ",i+1);
+                scanf("%d",&data);
+                enqueue(data);
+                i++;
+            }
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            loop=0;
+            break;
+        default:
+            printf("Invalid Option");
+            break;
+        }
+    }
+}
