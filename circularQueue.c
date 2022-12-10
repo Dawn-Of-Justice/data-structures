f2ff68c52de36ff19e1aa5d5843431302a0603de
Salo Soja Edwin <salosoja@gmail.com>
2022-12-11 02:04:37 +0530

    Added Cicular Queue

--- circularQueue.c
+++ circularQueue.c
@@ -1,33 +1,31 @@
 #include<stdio.h>
-#define size 20 //Size of the array
+#define size 5 //Size of the array
 int queue[size];
 int front = -1;
 int rear = -1;
 
 //Inserting data
 void enqueue(int x){
-    if(rear==size-1){
-        printf("Queue Overflow\n");
-    }else if(front==-1 && rear==-1){
-        front=rear=0;
-        queue[rear]=x;
+    if (front == -1 && rear == -1){
+        front = rear = 0;
+        queue[rear] = x;
+    }else if(((rear+1)%size)==front){
+        printf("Queue is full");
     }else{
-        rear++;
+        rear=(rear+1)%size;
         queue[rear]=x;
     }
 }
 
-//Deleting data 
 void dequeue(){
     if(front == -1 && rear == -1){
-        printf("Queue Underflow\n");
-    }
-    else{
+        printf("Queue is empty");
+    }else{
         printf("Deleted %d from front\n",queue[front]);
         if(front == rear){
-            front = rear = -1;
+            front = rear =-1;
         }else{
-            front++;
+            front =(front+1)%size;
         }
     }
 }
@@ -37,17 +35,20 @@ void display(){
         printf("Queue Empty\n");
     }else{
         printf("Queue\n");
-        for(int i=front; i<rear+1;i++){
+        int i=front;
+        while(i!=rear){
             printf("%d ",queue[i]);
+            i=(i+1)%size;
         }
+        printf("%d",queue[rear]);
     }
 }
 
 void main(){
-    int loop=1;
+int loop=1;
     while(loop){
         int choice;
-        printf("\nWhich operation would you like to do in queue?\n");
+        printf("\nWhich operation would you like to do in Circular queue?\n");
         printf("1 : Inserte data in the rear\n");
         printf("2 : Delete data in the front\n");
         printf("3 : Display the queue\n");
